8b5395bc26ed0c199a29999958b56313dc3119a6
Igor Oparin <igorop3@gmail.com>
2023-05-23 05:06:54 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-/* eslint-disable no-await-in-loop */
-/* eslint-disable no-cond-assign */
-import whatsAppClient from "@green-api/whatsapp-api-client";
+import whatsAppClient from '@green-api/whatsapp-api-client'
 import { useEffect, useState } from 'react'
 import ContactsList from './components/contactsList/ContactsList'
 import Chat from './components/chat/Chat'
 import Login from './components/login/Login'
 import s from './App.module.css'
 
-// const API_URL = 'https://api.green-api.com'
-
 function App() {
-  // const [contacts, setContacts] = useState([])
   const [message, setMessage] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
   const [idInstance, setIdInstance] = useState('')
@@ -26,73 +21,23 @@ function App() {
   }
 
   useEffect(() => {
+    if (idInstance === '' || apiToken === '') return
+    ;(async () => {
 
-    if (idInstance !== '' && apiToken !== '') { (async () => {
+      const restAPI = whatsAppClient.restAPI({
+        idInstance,
+        apiTokenInstance: apiToken,
+      })
 
-      const restAPI = whatsAppClient.restAPI(({
-          idInstance,
-          apiTokenInstance: apiToken
-      }))
-  
       try {
-        console.log('111')
         await restAPI.webhookService.startReceivingNotifications()
         restAPI.webhookService.onReceivingMessageText((body) => {
-            console.log('onReceivingMessageText', body)
-            setMessage("a")
-             restAPI.webhookService.stopReceivingNotifications();
-            // console.log("Notifications is about to stop in 5 sec if no messages will be queued...")
+          setMessage(body.messageData.textMessageData.textMessage)
         })
-        restAPI.webhookService.onReceivingDeviceStatus((body) => {
-            console.log('onReceivingDeviceStatus', body)
-        })
-        restAPI.webhookService.onReceivingAccountStatus((body) => {
-            console.log('onReceivingAccountStatus', body)
-        })
-      } catch (e) {
-          console.log("error:123", e)
-          restAPI.webhookService.stopReceivingNotifications();
+      } catch (ex) {
+        console.log('er', ex.toString())
       }
-  
-      console.log("End")
-  
-  })();}
-    // let interval = ''
-    // if (idInstance !== '' && apiToken !== '') {
-    //   interval = setInterval(
-    //     () =>
-    //       fetch(
-    //         `${API_URL}/waInstance${idInstance}/ReceiveNotification/${apiToken}`
-    //       )
-    //         .then((resp) => resp.json())
-    //         .then((data) => {
-    //           if (data.body.messageData.typeMessage === 'textMessage') {
-    //             setContacts([...contacts, data.body.senderData.chatName])
-    //             setMessage(data.body.messageData.textMessageData.textMessage)
-    //           } else {
-    //             setContacts([...contacts, data.body.senderData.chatName])
-    //             setMessage(data.body.messageData.typeMessage)
-    //           }
-    //           return data
-    //         })
-    //         .then((data) => {
-    //           fetch(
-    //             `${API_URL}/waInstance${idInstance}/deleteNotification/${apiToken}/${data.receiptId}`,
-    //             {
-    //               method: 'DELETE',
-    //             }
-    //           )
-    //         })
-    //         .catch((e) => {
-    //           console.log('error', e.message)
-    //         }),
-    //     5000
-    //   )
-    // }
-    // return () => {
-    //   // restAPI.webhookService.stopReceivingNotifications();
-    //   // clearInterval(interval)
-    // }
+    })()
   }, [idInstance, apiToken])
   return (
     <div className={s.app}>
@@ -106,6 +51,7 @@ function App() {
                 <label htmlFor="phone-number">
                   Введите номер чтобы начать чат:
                   <input
+                    className={s.phone_input}
                     type="number"
                     name="phone-number"
                     value={phoneNumber}
