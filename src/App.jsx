5427451677ad9f2f097bbef2018c7277d66e50fb
Igor Oparin <igorop3@gmail.com>
2023-05-22 19:45:35 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
               )
             })
             .catch((e) => {
-              console.log('error', e.message)
+              console.log(e.message)
             }),
         5000
       )
